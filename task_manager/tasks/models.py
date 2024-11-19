407646b163b86ebe5cd0a94a7aff6b4efce4361a
DaniilIslamgaliev <dante9226@mail.ru>
2024-11-19 15:34:50 +0300

    Update models.py
    Add labels

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from task_manager.users.models import User
 from task_manager.statuses.models import Status
+from task_manager.labels.models import Labels
 from django.utils.translation import gettext
 
 
@@ -18,6 +19,14 @@ class Tasks(models.Model):
     executor = models.ForeignKey(
         User, blank=True, on_delete=models.PROTECT,
         related_name='executor', verbose_name=gettext('executor'),)
+    labels = models.ManyToManyField(
+        Labels, blank=True, related_name='labels', through='LabelTask',
+        verbose_name=gettext('labels'),)
 
     def __str__(self):
         return self.name
+
+
+class TaskLabels(models.Model):
+    tasks = models.ForeignKey(Tasks, on_delete=models.CASCADE)
+    labels = models.ForeignKey(Labels, on_delete=models.RESTRICT)
